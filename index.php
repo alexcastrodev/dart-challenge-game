ef96a702d6de24f12b2240b61ae9b764d8d14ed9
Alex castro <alexoliveira7x@gmail.com>
2020-05-08 18:45:17 -0300

    work in progress

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-
 use Siler\{Http\Response, Route};
 use App\Controllers\Leasons;
+
+Response\header('Access-Control-Allow-Origin', '*');
+Response\header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
 
 Route\get('/',  function() {
     return Response\json(['Hello' => 'What are you looking for?']);
