844ce656e51ff17fc9d9ab324bf98c870d1a5b20
Alex castro <alexoliveira7x@gmail.com>
2020-05-28 09:27:42 -0300

    fix cors

--- public/index.php
+++ public/index.php
@@ -1,4 +1,23 @@
 <?php
+
+$isOptions = strtoupper($_SERVER['REQUEST_METHOD']) == 'OPTIONS';
+$isSapiServer = in_array(strtolower(php_sapi_name()), ['cli-server', 'cli']);
+
+// Cors Skip. All Headers are send by .htaccess
+if ($isSapiServer) {
+  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS');
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type, Accept, Authorization, X-Requested-With, Origin');
+  header('Access-Control-Max-Age: ' . (12 * 24 * 60));
+
+  // Only Cors Request
+  if ($isOptions) {
+    http_response_code(204);
+    exit;
+  }
+}
+
+
 require_once '../vendor/autoload.php';
 require "../config/bootstrap.php";
 
